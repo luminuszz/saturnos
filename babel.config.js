9762a03943e53c35d22c1d802d233bd8c8966880
Davi Ribeiro <davibens4@hotmail.com>
2021-11-04 15:19:52 -0300

    feat(timer): Ajuste timer time-zone

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    presets: [
-        [
-            "@babel/preset-env",
-            {
-                targets: {
-                    node: "current",
-                },
-            },
-        ],
-        "@babel/preset-typescript",
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '12',
+        },
+      },
     ],
-    plugins: [
-        "babel-plugin-transform-typescript-metadata",
-        ["@babel/plugin-proposal-decorators", { legacy: true }],
-        ["@babel/plugin-proposal-class-properties", { loose: true }],
-    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
 };
